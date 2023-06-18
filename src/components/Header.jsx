b032b0a87ba0f5d7dc0abb4bd2be34fddb9630eb
Hanif Nugraha <nailavmaerd@gmail.com>
2023-06-18 15:53:51 +0700

    feat: update prettier option, react router, button

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,52 +1,43 @@
-import React from "react";
-import * as Icon from "react-feather";
+import React from 'react';
+import * as Icon from 'react-feather';
+import { Link } from 'react-router-dom';
 
 const Header = () => {
-  // const [activePage, setActivePage] = useState("");
-
-  // const handleNavLinkClick = (page) => {
-  //   setActivePage(page);
-  // };
-
   return (
     <nav>
-      <a href="/#" className="site-title">
+      <Link to='/' className='site-title'>
         Hanif <br />
         Nugraha <br />
-      </a>
-      <ul className="nav-header">
+      </Link>
+      <ul className='nav-header'>
         <li>
-          {/* <NavLink
-              exact
-              to="/"
-              activeClassName="active"
-              onClick={() => handleNavLinkClick("projects")}
-            >
-              projects
-            </NavLink> */}
-          <a href="/#" className="nav-item">
+          <Link to='/projects' className='nav-item'>
             projects
-          </a>
+          </Link>
         </li>
         <li>
-          <a href="/#" className="nav-item">
+          <Link to='/visuals' className='nav-item'>
             visuals
-          </a>
+          </Link>
         </li>
         <li>
-          <a href="/#" className="nav-item">
+          <Link to='/resume' className='nav-item'>
             resume
-          </a>
+          </Link>
         </li>
         <li>
-          <a href="/#" className="nav-item">
+          <Link to='/about' className='nav-item'>
             about
-          </a>
+          </Link>
         </li>
-        <a href="/#" className="nav-icon">
+        <a
+          href='https://www.instagram.com/hanifnnugraha_/'
+          className='nav-icon'>
           <Icon.Instagram strokeWidth={1} />
         </a>
-        <a href="/#" className="nav-icon">
+        <a
+          href='https://www.linkedin.com/in/hanifnugraha/'
+          className='nav-icon'>
           <Icon.Linkedin strokeWidth={1} />
         </a>
       </ul>
--- src/pages/home/Hero.jsx
+++ src/pages/home/Hero.jsx
@@ -1,12 +1,12 @@
-import { AnimatePresence, motion } from "framer-motion";
-import React, { useState } from "react";
-import * as Icon from "react-feather";
-import { Link } from "react-router-dom";
-import MyImage from "../../assets/image1.jpeg";
+import { AnimatePresence, motion } from 'framer-motion';
+import React, { useState } from 'react';
+import * as Icon from 'react-feather';
+import { Link } from 'react-router-dom';
+import MyImage from '../../assets/image1.jpeg';
 
 const Hero = () => {
   const [isOpen, setIsOpen] = useState(false);
-  const [email, setEmail] = useState("");
+  const [email, setEmail] = useState('');
   const [icon, setIcon] = useState(<Icon.ArrowRightCircle strokeWidth={1} />);
 
   const toggleOpen = () => {
@@ -20,40 +20,37 @@ const Hero = () => {
     );
   };
 
-  const handleSubmit = (e) => {
+  const handleSubmit = e => {
     e.preventDefault();
     console.log(email);
   };
 
-  const handleChange = (e) => {
+  const handleChange = e => {
     setEmail(e.target.value);
   };
 
   return (
-    <section className="hero" id="hero">
-      <div className={`left-hero ${isOpen ? "open" : ""}`} id="left-hero">
+    <section className='hero' id='hero'>
+      <div className={`left-hero ${isOpen ? 'open' : ''}`} id='left-hero'>
         <motion.div
-          className="cover-hero"
-          id="cover-hero"
+          className='cover-hero'
+          id='cover-hero'
           initial={{ opacity: 0, x: -32 }}
           animate={{ opacity: 1, x: 0 }}
-          exit={{ opacity: 0, x: 32 }}
-        >
-          <img className="left-hero-cover" src={MyImage} alt="Cover 1" />
-          <img className="left-hero-cover-2" src={MyImage} alt="Cover 2" />
+          exit={{ opacity: 0, x: 32 }}>
+          <img className='left-hero-cover' src={MyImage} alt='Cover 1' />
+          <img className='left-hero-cover-2' src={MyImage} alt='Cover 2' />
         </motion.div>
         <motion.h2
           initial={{ opacity: 0, y: -32 }}
           animate={{ opacity: 1, y: 0 }}
-          exit={{ opacity: 0, y: 32 }}
-        >
+          exit={{ opacity: 0, y: 32 }}>
           Greetings! I'm Hanif
         </motion.h2>
         <motion.p
           initial={{ opacity: 0, x: -32 }}
           animate={{ opacity: 1, x: 0 }}
-          exit={{ opacity: 0, x: 32 }}
-        >
+          exit={{ opacity: 0, x: 32 }}>
           I'm a student of Information System Bachelor's Degree interested in
           <strong> UI/UX & Front-End Development.</strong>
           <br />
@@ -66,33 +63,30 @@ const Hero = () => {
         <motion.p
           initial={{ opacity: 0, x: 32 }}
           animate={{ opacity: 1, x: 0 }}
-          exit={{ opacity: 0, x: -32 }}
-        >
-          Check out <Link to="#">about me</Link> or see my{" "}
-          <Link to="#">resume</Link> here.
+          exit={{ opacity: 0, x: -32 }}>
+          Check out <Link to='#'>about me</Link> or see my{' '}
+          <Link to='#'>resume</Link> here.
         </motion.p>
       </div>
-      <div className="right-hero" id="right-hero">
+      <div className='right-hero' id='right-hero'>
         <motion.button
-          className="toggle-button"
+          className='toggle-button'
           onClick={toggleOpen}
           initial={{ opacity: 0, x: -8 }}
           animate={{ opacity: 1, x: 0 }}
-          exit={{ opacity: 0, x: -8 }}
-        >
+          exit={{ opacity: 0, x: -8 }}>
           {icon}
         </motion.button>
         <AnimatePresence>
           {isOpen && (
             <motion.div
-              className="content"
+              className='content'
               initial={{ opacity: 0, x: -32 }}
               animate={{ opacity: 1, x: 0 }}
-              exit={{ opacity: 0, x: 64 }}
-            >
-              <div className="right-hero-content">
+              exit={{ opacity: 0, x: 64 }}>
+              <div className='right-hero-content'>
                 <h3>Here's my contact</h3>
-                <p className="right-hero-content">
+                <p className='right-hero-content'>
                   <span>
                     <Icon.Instagram strokeWidth={1} />
                     @hanifnnugraha_
@@ -108,15 +102,15 @@ const Hero = () => {
                     <Icon.Mail strokeWidth={1} />
                     hanifnugraha69@gmail.com
                   </span>
-                  <form className="email-form" onSubmit={handleSubmit}>
+                  <form className='email-form' onSubmit={handleSubmit}>
                     <input
-                      className="email-input"
-                      type="email"
-                      placeholder="Enter your email"
+                      className='email-input'
+                      type='email'
+                      placeholder='Enter your email'
                       value={email}
                       onChange={handleChange}
                     />
-                    <button className="submit-button" type="submit">
+                    <button className='submit-button' type='submit'>
                       Submit
                     </button>
                   </form>
